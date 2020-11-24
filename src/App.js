d5ba67c95ff4db77756810823264bab010590d89
Sanjana97k <sanjana.sarkar@hybrowlabs.com>
2020-11-24 10:13:10 +0530

    initial ui build

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import HomePage from './homepage/homepage.component.jsx'
+import './homepage/homepage.styles.scss'
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
